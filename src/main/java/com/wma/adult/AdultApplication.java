35133a64127aecd0cb4441e3d792d4b0df762f39
MingAo.Wang <490746560@qq.com>
2020-10-19 14:15:10 +0800

    create program

---
+++ src/main/java/com/wma/adult/AdultApplication.java
@@ -0,0 +1,13 @@
+package com.wma.adult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wma/adult/AdultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wma.adult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
